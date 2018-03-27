2b111496e17764d99029c636d0b860e45ed96d21
MuriloBSD <murilo@la804.com.br>
2018-03-27 17:10:01 -0300

    create station and testing it

--- project/api/v1/stations.py
+++ project/api/v1/stations.py
@@ -1,8 +1,49 @@
-from flask import jsonify
+from sqlalchemy import exc
+from flask import jsonify, request
+from flask_classful import route
+
 from project.api.v1 import V1FlaskView
+from project.api.schematics import add_station_schema
+from project.extensions import db
+from project.api.models import Station
 
 
 class StationsView(V1FlaskView):
 
     def index(self):
         return jsonify({'message': 'ok'})
+
+    @route('ping')
+    def ping(self):
+        return jsonify({
+            'status': 'success',
+            'message': 'pong!'
+        })
+
+    def post(self):
+        response_obj = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        json_data = request.get_json()
+
+        if not json_data:
+            return jsonify(response_obj), 400
+
+        data, errors = add_station_schema.load(json_data)
+
+        if errors:
+            response_obj['errors'] = errors
+            return jsonify(response_obj), 422
+
+        try:
+            db.session.add(Station(
+                name=data['name'], latitude=data['latitude'],
+                longitude=data['longitude']))
+            db.session.commit()
+            response_obj['status'] = 'success'
+            response_obj['message'] = f'{data["name"]} was added!'
+            return jsonify(response_obj), 201
+        except (exc.IntegrityError, ValueError) as e:
+            db.session.rollback()
+            return jsonify(response_obj), 400
--- project/tests/test_stations.py
+++ project/tests/test_stations.py
@@ -16,6 +16,23 @@ class TestStationsService(BaseTestCase):
         self.assertIn('pong!', data['message'])
         self.assertIn('success', data['status'])
 
+    def test_add_station(self):
+        """Ensure a new station can be added to the database."""
+        with self.client:
+            response = self.client.post(
+                f'{self.version}stations',
+                data=json.dumps({
+                    'name': 'estacao-x',
+                    'latitude': -15.789343,
+                    'longitude': -47.925756
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('estacao-x was added', data['message'])
+            self.assertIn('success', data['status'])
+
 
 if __name__ == '__main__':
     unittest.main()
