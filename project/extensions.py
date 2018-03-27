7be86c57900b5a9d01ccd1349ba098bd57321a82
MuriloBSD <murilo@la804.com.br>
2018-03-27 17:09:18 -0300

    add marshmallow extension

--- project/extensions.py
+++ project/extensions.py
@@ -1,8 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 
 # db
 db = SQLAlchemy()
 
 # migrate
 migrate = Migrate()
+
+marshmallow = Marshmallow()
