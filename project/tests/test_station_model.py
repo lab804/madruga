a638de8b24edc937f407e3a120c1de2f8b171396
MuriloBSD <murilo@la804.com.br>
2018-03-27 16:22:30 -0300

    test add url station

--- project/tests/test_station_model.py
+++ project/tests/test_station_model.py
@@ -15,6 +15,16 @@ class TestUserModel(BaseTestCase):
         self.assertIsNone(station.url)
         self.assertTrue(station.is_public)
 
+    def test_add_station_url(self):
+        station = add_station(
+            'estacao-x', -15.789343, -47.925756, 'http://labmet.com.br')
+        self.assertTrue(station.id)
+        self.assertEqual(station.name, 'estacao-x')
+        self.assertEqual(station.latitude, -15.789343)
+        self.assertEqual(station.longitude, -47.925756)
+        self.assertEqual(station.url, 'http://labmet.com.br')
+        self.assertTrue(station.is_public)
+
 
 if __name__ == '__main__':
     unittest.main()
