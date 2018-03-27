444bb4345d088836ffb86126da3700c170f98bb8
MuriloBSD <murilo@la804.com.br>
2018-03-27 17:45:22 -0300

    add minimum distance to find station

--- project/config.py
+++ project/config.py
@@ -6,6 +6,7 @@ class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    MINIMUM_DISTANCE = 100
 
 
 class DevelopmentConfig(BaseConfig):
